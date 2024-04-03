57f21288e711501301afb1bf02fd0dc85a11ce78
Kaio Silveira <silveira.kaio@icloud.com>
2024-04-03 05:38:43 +0200

    update `Order` to resolve `Customer` from repository

--- src/order/index.js
+++ src/order/index.js
@@ -1,13 +1,12 @@
-import { Customer } from '../customer';
-
 export class Order {
   constructor(data) {
     this._number = data.number;
-    this._customer = new Customer(data.customer);
+    this._customer = data.customer;
+    this._customerRepository = data.customerRepository;
     // load other data
   }
 
   get customer() {
-    return this._customer;
+    return this._customerRepository.find(this._customer);
   }
 }
--- src/order/index.test.js
+++ src/order/index.test.js
@@ -1,8 +1,16 @@
+import { CustomerRepository } from '../customer-repository';
 import { Order } from './index';
-
 describe('Order', () => {
   it('should have a customer', () => {
-    const order = new Order({ number: '123', customer: '456' });
+    CustomerRepository.initialize();
+    CustomerRepository.register('456');
+
+    const order = new Order({
+      number: '123',
+      customer: '456',
+      customerRepository: CustomerRepository,
+    });
+
     expect(order.customer.id).toBe('456');
   });
 });
